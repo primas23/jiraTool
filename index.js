766edee4210d970ecdafbdee5a8680ca19a0049e
Petar Petrov <petpet@cobuilder.no>
2018-03-09 16:35:27 +0200

    extracted modules

--- index.js
+++ index.js
@@ -1,61 +1,19 @@
 const http = require('http');
-const command = 'git log --pretty=format:"%ce;%s;%ad" --date=short';
-const location = 'D:\\development\\cobuilder-gobim';
-const newLineDevider = '\r\n';
-
-let spawn = require('child_process').spawn,
-    child;
+let logReader = require('./utils/logsReader.js');
+let constants = require('./common/constants.js');
 
 http.createServer((req, res) => {
-    printLogPromise
+    logReader
         .then((logs) => {
-            res.writeHead(200, {
-                'Content-Type': 'text/plain'
-            });
-            res.write(`Logs for ${location}`);
+            res.writeHead(200, constants.contentTypeText);
+            res.write(`Logs for ${constants.location}`);
             res.write(logs);
             res.end();
         })
         .catch((err) => {
-            res.writeHead(500, {
-                'Content-Type': 'text/plain'
-            });
-            rres.write(`Errors for ${location}`);
+            res.writeHead(500, constants.contentTypeText);
+            res.write(`Errors for ${constants.location}`);
             res.write(err);
             res.end();
         });
-}).listen(2205);
-
-var printLogPromise = new Promise((resolve, reject) => {
-    var allLogs = '',
-        allErrors = '';
-    child = spawn('powershell.exe', [`cd ${location}; ${command}`]);
-    child.stdout.on('data', data => {
-        allLogs += `Logs Data: ${data} ${newLineDevider}`;
-    });
-
-    child.stderr.on('data', data => {
-        allErrors += `Logs Errors: ${data} ${newLineDevider}`;
-    });
-
-    child.on('exit', () => {
-        resolve(allLogs);
-        reject(allErrors);
-    });    
-});
-
-/*
-function printLog() {
-    child = spawn('powershell.exe', [`cd ${location}; ${command}`]);
-    child.stdout.on('data', data => {
-        return `Powershell Data: ${data}`;
-    });
-    child.stderr.on('data', data => {
-        return `Powershell Errors: ${data}`;
-    });
-    child.on('exit', () => {
-        return 'Powershell Script finished';
-    });
-    return child.stdin.end();
-}
-*/
+}).listen(constants.port);
---
+++ utils/logsReader.js
@@ -0,0 +1,23 @@
+let constants = require('../common/constants.js');
+let spawn = require('child_process').spawn,
+    child;
+
+let logReader = new Promise((resolve, reject) => {
+    let allLogs = '',
+        allErrors = '';
+    child = spawn('powershell.exe', [`cd ${constants.location}; ${constants.command}`]);
+    child.stdout.on('data', data => {
+        allLogs += `Logs Data: ${data} ${constants.newLineDevider}`;
+    });
+
+    child.stderr.on('data', data => {
+        allErrors += `Logs Errors: ${data} ${constants.newLineDevider}`;
+    });
+
+    child.on('exit', () => {
+        resolve(allLogs);
+        reject(allErrors);
+    });    
+});
+
+module.exports = logReader;
